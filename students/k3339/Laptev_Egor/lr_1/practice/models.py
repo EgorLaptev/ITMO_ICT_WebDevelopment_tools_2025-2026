1b287770adb8d286b7a4e9c86d10518de390af0e
Egor Laptev <egor.lapteffff@gmail.com>
2026-04-10 18:08:49 +0300

    Merge branch 'feat/lr1-practice-2' into feat/lr1

--- students/k3339/Laptev_Egor/lr_1/practice/models.py
+++ students/k3339/Laptev_Egor/lr_1/practice/models.py
@@ -1,28 +1,49 @@
 from enum import Enum
 from typing import Optional, List
+from sqlmodel import SQLModel, Field, Relationship
 
-from pydantic import BaseModel
 
 class RaceType(Enum):
     director = "director"
     worker = "worker"
     junior = "junior"
 
-class Warrior(BaseModel):
-    id: int
+
+class SkillWarriorLink(SQLModel, table=True):
+    skill_id: Optional[int] = Field(default=None, foreign_key="skill.id", primary_key=True)
+    warrior_id: Optional[int] = Field(default=None, foreign_key="warrior.id", primary_key=True)
+
+
+class Skill(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    description: Optional[str] = ""
+    warriors: Optional[List["Warrior"]] = Relationship(
+        back_populates="skills", link_model=SkillWarriorLink
+    )
+
+
+class Profession(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    title: str
+    description: str
+    warriors_prof: List["Warrior"] = Relationship(back_populates="profession")
+
+
+class WarriorDefault(SQLModel):
     race: RaceType
     name: str
     level: int
-    profession: Profession
-    skills: Optional[List[Skill]] = []
-
-class Profession(BaseModel):
-    id: int
-    title: str
-    description: str
+    profession_id: Optional[int] = Field(default=None, foreign_key="profession.id")
 
 
-class Skill(BaseModel):
-    id: int
-    name: str    
-    description: str
+class Warrior(WarriorDefault, table=True):
+    id: int = Field(default=None, primary_key=True)
+    profession: Optional[Profession] = Relationship(back_populates="warriors_prof")
+    skills: Optional[List[Skill]] = Relationship(
+        back_populates="warriors", link_model=SkillWarriorLink
+    )
+
+
+class WarriorProfessions(WarriorDefault):
+    profession: Optional[Profession] = None
