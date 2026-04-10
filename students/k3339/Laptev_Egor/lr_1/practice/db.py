f47c20bab8416f1f9eb3edcf42fd411dbf609675
Egor Laptev <egor.lapteffff@gmail.com>
2026-04-10 18:00:59 +0300

    feat: practice work 2 completed

--- students/k3339/Laptev_Egor/lr_1/practice/db.py
+++ students/k3339/Laptev_Egor/lr_1/practice/db.py
@@ -1,11 +1,17 @@
+import os
 from sqlmodel import SQLModel, Session, create_engine
 
-db_url = 'postgresql://postgres:123@localhost/warriors_db'
-engine = create_engine(db_url, echo=True)
+
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:123@localhost/warriors_db"
+)
+engine = create_engine(DATABASE_URL, echo=True)
+
 
 def init_db():
     SQLModel.metadata.create_all(engine)
-
+    
 
 def get_session():
     with Session(engine) as session:
--- students/k3339/Laptev_Egor/lr_1/practice/models.py
+++ students/k3339/Laptev_Egor/lr_1/practice/models.py
@@ -30,13 +30,20 @@ class Profession(SQLModel, table=True):
     warriors_prof: List["Warrior"] = Relationship(back_populates="profession")
 
 
-class Warrior(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
+class WarriorDefault(SQLModel):
     race: RaceType
     name: str
     level: int
     profession_id: Optional[int] = Field(default=None, foreign_key="profession.id")
+
+
+class Warrior(WarriorDefault, table=True):
+    id: int = Field(default=None, primary_key=True)
     profession: Optional[Profession] = Relationship(back_populates="warriors_prof")
     skills: Optional[List[Skill]] = Relationship(
         back_populates="warriors", link_model=SkillWarriorLink
     )
+
+
+class WarriorProfessions(WarriorDefault):
+    profession: Optional[Profession] = None
