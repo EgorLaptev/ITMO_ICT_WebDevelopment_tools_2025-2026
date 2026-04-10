c78e0acf279535386b1e8236e64331785487bce7
Egor Laptev <egor.lapteffff@gmail.com>
2026-04-10 19:40:24 +0300

    fix: update Dockerfile CMD and import paths in main.py for correct module references

--- students/k3339/Laptev_Egor/lr_1/practice/app/main.py
+++ students/k3339/Laptev_Egor/lr_1/practice/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException
 from typing import List, TypedDict
 from sqlmodel import select
-from practice.app.models import Warrior, WarriorDefault, Profession, WarriorProfessions
-from practice.app.db import init_db, get_session
+from app.models import Warrior, WarriorDefault, Profession, WarriorProfessions
+from app.db import init_db, get_session
 
 app = FastAPI()
 
